6d07c755bdc89229680ec7e858ae1b05ae81ded2
Brandon Burke <brandonburke93@outlook.com>
2019-03-25 19:07:13 -0600

    Add classes to test CSV
    Add  Employee.cs and Process.cs to test using CSV file instead of a database.

---
+++ SalaryInversion/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryInversion
+{
+    class Employee
+    {
+        private string College { get; set; }
+        private string Department { get; set; }
+        private string Name { get; set; }
+        private string Rank { get; set; }
+        private int Salary { get; set; }
+
+        public Employee(string college, string department, string name, string rank, int salary)
+        {
+            this.College = college;
+            this.Department = department;
+            this.Name = name;
+            this.Rank = rank;
+            this.Salary = salary;
+        }
+    }
+}
---
+++ SalaryInversion/Process.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SalaryInversion
+{
+    class Process
+    {
+        /// <summary>
+        /// Contains all inverted employees, after calling GetEmployees
+        /// </summary>
+        private static List<Employee> employees = new List<Employee>();
+
+        /// <summary>
+        /// Loads the list of inverted employees from csv file
+        /// </summary>
+        private static void GetEmployees()
+        {
+            var input = File.ReadAllLines("../../Data/2019eqmodelabbreviated.csv");
+            foreach (string line in input)
+            {
+                var values = line.Split(',');
+
+                // If statement simply skips the header row from the CSV file
+                if (values[0] == "CLG")
+                {
+                    continue;
+                }
+
+                // Seperate values out for easier reading
+                string college = values[0];
+                string department = values[1];
+                string name = values[2].Replace("\"", "") + ", " + values[3].Replace("\"", "");
+                string rank = values[4];
+                int salary = int.Parse(values[5]);
+
+                employees.Add(new Employee(college, department, name, rank, salary));
+            }
+        }
+
+        public static void Main()
+        {
+            GetEmployees();
+            Console.WriteLine("Yeah, I'm right here.");
+            Console.Read();
+        }
+    }
+}
